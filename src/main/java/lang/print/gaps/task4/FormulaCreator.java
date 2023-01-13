3639b15c98cc042a015bb40d786ba39a31718bed
samandaribragimov <samandar0406@gmail.com>
2023-01-13 17:03:40 +0100

    Done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        long result = (9 * a * a - 5 * b + a - 5) * (a + b - 4 * a * b) / 2;
+
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,6 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.println('\u0063'+""+'\u0068'+""+'\u0061'+""+'\u0072');
     }
 }
